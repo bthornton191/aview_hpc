12d0135abf86e0fbc9e938a210fb3d986c1ee40e
Ben Thornton <ben.thornton@hexagon.com>
2024-12-10 17:32:05 -0500

    Bump version to 0.2.11 and update version notes

--- aview_hpc/version.py
+++ aview_hpc/version.py
@@ -1,3 +1,3 @@
-version = '0.2.10'
-version_notes = 'Remove debug print statement from get_binary_version function'
+version = '0.2.11'
+version_notes = 'Add resubmit_job command and get_job_table function to CLI'
 date = 'December, 10th 2024'
