9a4211afde3dbb890cbb505d953e47a6bb0aecf9
Ben Thornton <ben.thornton@hexagon.com>
2024-12-10 15:34:23 -0500

    Bump version to 0.2.9 and update version notes

--- aview_hpc/version.py
+++ aview_hpc/version.py
@@ -1,3 +1,3 @@
-version = '0.2.8'
-version_notes = 'Add `get_binary` command to CLI'
+version = '0.2.9'
+version_notes = 'Fix download URL for Windows binary in get_binary.py'
 date = 'December, 10th 2024'
