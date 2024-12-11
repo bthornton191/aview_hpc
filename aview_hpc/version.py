ade7e11b254805bdf13f6bb75ed54427c09ed20c
Ben Thornton <ben.thornton@hexagon.com>
2024-12-11 12:21:08 -0500

    Bump version to 0.2.13 and update version notes to enhance job resubmission functionality

--- aview_hpc/version.py
+++ aview_hpc/version.py
@@ -1,4 +1,3 @@
-version = '0.2.12'
-version_notes = ('Fix binary version check and improve logging for download process. '
-                 'Add resubmit_job function to facilitate job resubmission in remote directories')
+version = '0.2.13'
+version_notes = ('Enhance job resubmission functionality with completion check and output handling. ')
 date = 'December, 11th 2024'
