b7ec3bd7f445358b3af2c934eb9a7f847cdd63bf
Ben Thornton <ben.thornton@hexagon.com>
2024-12-11 11:21:46 -0500

    Update version date to December 11th, 2024 and adjust version notes

--- aview_hpc/version.py
+++ aview_hpc/version.py
@@ -1,4 +1,4 @@
 version = '0.2.12'
 version_notes = ('Fix binary version check and improve logging for download process. '
                  'Add resubmit_job function to facilitate job resubmission in remote directories')
-date = 'December, 10th 2024'
+date = 'December, 11th 2024'
