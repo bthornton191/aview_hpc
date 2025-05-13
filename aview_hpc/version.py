851618dec7483e7cfb202592237c6eb31c3994b0
Ben Thornton <ben.thornton@hexagon.com>
2025-05-13 09:58:43 -0400

    Update release date in version.py to May 13th, 2025

--- aview_hpc/version.py
+++ aview_hpc/version.py
@@ -1,3 +1,3 @@
 version = '0.2.18'  # REDUNDANT: MAKE SURE THIS MATCHES THE VERSION setup.py
 version_notes = ('Improve binary version checking logic to fix error when binary does not exist.')
-date = 'May, 9th 2025'
+date = 'May, 13th 2025'
